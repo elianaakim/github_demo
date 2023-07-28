2dfc293ff9e61e84ce88a0686b6661ea710ad07f
Eliana Kim <yunmin626@gmail.com>
2023-07-28 10:26:26 -0700

    Initial Commit

---
+++ github_demo/github_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_demoApp.swift
+//  github_demo
+//
+//  Created by Eliana kim on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct github_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
